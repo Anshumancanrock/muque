0c8248f313c4ceab6c3204b5fe5dacad6cdd72c7
anshumancanrock <anshu.1239.as@gmail.com>
2026-02-10 11:33:29 +0530

    feat: add in-memory broker with topic subscriber map

--- broker.go
+++ broker.go
@@ -1,7 +1,16 @@
 package main
 
+import (
+	"net"
+)
+
 type Message struct {
 	Command string `json:"command"`
 	Topic   string `json:"topic"`
 	Payload string `json:"payload"`
+}
+
+type Broker struct {
+	// Topic -> connections subscribed to that topic
+	Subscribers map[string][]net.Conn
 }
