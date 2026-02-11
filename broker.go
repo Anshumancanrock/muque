9c6fad57438631f5112618be1875435961ab679e
anshumancanrock <anshu.1239.as@gmail.com>
2026-02-11 17:02:14 +0530

    feat: subscribe TCP connections to a topic

--- broker.go
+++ broker.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net"
 )
 
@@ -13,4 +14,9 @@ type Message struct {
 type Broker struct {
 	// Topic -> connections subscribed to that topic
 	Subscribers map[string][]net.Conn
+}
+
+func (b *Broker) Subscribe(topic string, conn net.Conn) {
+	b.Subscribers[topic] = append(b.Subscribers[topic], conn)
+	fmt.Printf("New subscriber added to topic: %s\n", topic)
 }
