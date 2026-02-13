1aca16255daa7190b27d486d78598894ac0826e9
anshumancanrock <anshu.1239.as@gmail.com>
2026-02-13 15:54:03 +0530

    feat: broadcast payloads to topic subscribers

--- broker.go
+++ broker.go
@@ -20,3 +20,14 @@ func (b *Broker) Subscribe(topic string, conn net.Conn) {
 	b.Subscribers[topic] = append(b.Subscribers[topic], conn)
 	fmt.Printf("New subscriber added to topic: %s\n", topic)
 }
+
+func (b *Broker) Publish(topic string, payload string) {
+	subscribers := b.Subscribers[topic]
+	outgoingMsg := []byte(fmt.Sprintf("BROKER BROADCAST [%s]: %s\n", topic, payload))
+
+	for _, conn := range subscribers {
+		conn.Write(outgoingMsg)
+	}
+
+	fmt.Printf("Broadcasted to %d subscribers on topic: %s\n", len(subscribers), topic)
+}
